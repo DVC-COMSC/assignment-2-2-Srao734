47618c99b5bdc624369c417a13e100d45699ad44
Srao734 <136876420+Srao734@users.noreply.github.com>
2023-06-23 19:06:22 +0000

    celcius to fahrenheit converter

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 def main():
-    """
-    ##################################################
-    # Comlete your code here
-    Use the same variables: celcius fahrenheit 
-    ##################################################
-    """
-
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
+    
+    celcius = input('Enter Celcius: ')
+    fahrenheit = (9/5) * float(celcius) + 32
+    print (f'Fahrenheit: \t {fahrenheit:.2f}')
+    
+    
+    
+    
+    
     return celcius, fahrenheit
+
 
 
 if __name__ == '__main__':
